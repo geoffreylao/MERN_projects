ccd657ae6ebe5c19606e7c6bdbfded82c4e3148c
geoffrey <geoffreylao@outlook.com>
2021-06-02 23:22:05 -0300

    moves getstats to backend, saved performance

--- bezkoder-slippi-mern/node-js-mongodb/app/config/db.config.js
+++ bezkoder-slippi-mern/node-js-mongodb/app/config/db.config.js
@@ -1,4 +1,14 @@
 module.exports = {
-  //url: "mongodb+srv://slpuser:slppassword@mongoslp.ginsc.mongodb.net/mongoslp"
+  //url: "mongodb+srv://slpuser:slppassword@Clusterslp.ginsc.mongodb.net/mongoslp"
   url: 'mongodb://127.0.0.1:27017/mongoslp'
 }
+
+// db speed test
+
+// atlas
+// my records - 4.74s
+// kyles records - 1.37 min
+
+// localhost
+// my records - 1.85s
+// kyles records - 11.58s
